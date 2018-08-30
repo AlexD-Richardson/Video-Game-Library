c4ca23444a6be53d7bb7a215ffe0f0523f62c831
Alex Richardson <alexrichardson@macbook-pro-69.local>
2018-08-30 10:47:43 -0500

    Initial Commit

---
+++ Video Game Library/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library
+//
+//  Created by Alex Richardson on 8/30/18.
+//  Copyright © 2018 Alex Richardson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
